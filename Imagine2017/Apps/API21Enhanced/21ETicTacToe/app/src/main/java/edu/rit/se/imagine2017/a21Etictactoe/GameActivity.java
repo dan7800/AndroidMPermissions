77d85068c671b43e09445be58f7d56fbda9da7aa
shehan peruma <shehan.peruma@gmail.com>
2017-05-02 22:01:59 -0400

    added app for 'enhanced' permissions

---
+++ Imagine2017/Apps/API21Enhanced/21ETicTacToe/app/src/main/java/edu/rit/se/imagine2017/a21Etictactoe/GameActivity.java
@@ -0,0 +1,353 @@
+package edu.rit.se.imagine2017.a21Etictactoe;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class GameActivity extends AppCompatActivity {
+
+    final private int REQUEST_PERMISSION_CODE = 1122;
+    String userID;
+
+    private State currentPlayer;
+    private int colorCurrentPlayer,colorWinningBackground,colorDefaultBackground;
+    private boolean gameCompleted, vsComputer;
+    private int availableSquares;
+
+    private String[] friends = {"Fred","Mary","Dave","Lisa","Mark","Pete"};
+
+    private State[][] squares;
+
+    TextView textGameStatus;
+
+    Button btn00,btn01,btn02;
+    Button btn10,btn11,btn12;
+    Button btn20,btn21,btn22;
+    Button btnNewGame;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        btn00 = (Button) findViewById(R.id.button00);
+        btn01 = (Button) findViewById(R.id.button01);
+        btn02 = (Button) findViewById(R.id.button02);
+
+        btn10 = (Button) findViewById(R.id.button10);
+        btn11 = (Button) findViewById(R.id.button11);
+        btn12 = (Button) findViewById(R.id.button12);
+
+        btn20 = (Button) findViewById(R.id.button20);
+        btn21 = (Button) findViewById(R.id.button21);
+        btn22 = (Button) findViewById(R.id.button22);
+
+        textGameStatus = (TextView) findViewById(R.id.textGameStatus);
+        btnNewGame = (Button) findViewById(R.id.buttonNewGame);
+
+
+
+        btnNewGame.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        initializeGame();
+                    }
+                }
+        );
+
+        initializeGame();
+    }
+    private void systemMove(){
+        int row,column;
+
+        while (true) {
+            row = (int) (Math.random() * 3 + 0);
+            column = (int) (Math.random() * 3 + 0);
+            if(squares[row][column] == State.Blank)
+            {
+                String buttonTag = "["+row+"]["+column+"]";
+
+                TableLayout grid = (TableLayout) findViewById(R.id.tableLayout1);
+                int rowCount = grid.getChildCount();
+                for(int i=0;i<rowCount;i++){
+                    View rowView = grid.getChildAt(i);
+                    if(rowView instanceof TableRow){
+                        for(int j=0;j<((TableRow) rowView).getChildCount();j++){
+                            View v = ((TableRow) rowView).getChildAt(j);
+                            if (v instanceof Button){
+                                if(v.getTag()!=null && v.getTag().equals(buttonTag)) {
+                                    onClick(v);
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void onClick(View v) {
+
+        if (!gameCompleted) {
+
+            Button button = (Button) v;
+
+            button.setText(currentPlayer.toString());
+            button.setTextColor(colorCurrentPlayer);
+            button.setClickable(false);
+
+            if(btn00.getId() == v.getId()){
+                squares[0][0] = currentPlayer;
+                availableSquares--;
+            }
+            else if(btn01.getId() == v.getId()){
+                squares[0][1] = currentPlayer;
+                availableSquares--;
+            }
+            else if(btn02.getId() == v.getId()){
+                squares[0][2] = currentPlayer;
+                availableSquares--;
+            }
+            else if(btn10.getId() == v.getId()){
+                squares[1][0] = currentPlayer;
+                availableSquares--;
+            }
+            else if(btn11.getId() == v.getId()){
+                squares[1][1] = currentPlayer;
+                availableSquares--;
+            }
+            else if(btn12.getId() == v.getId()){
+                squares[1][2] = currentPlayer;
+                availableSquares--;
+            }
+            else if(btn20.getId() == v.getId()){
+                squares[2][0] = currentPlayer;
+                availableSquares--;
+            }
+            else if(btn21.getId() == v.getId()){
+                squares[2][1] = currentPlayer;
+                availableSquares--;
+            }
+            else if(btn22.getId() == v.getId()){
+                squares[2][2] = currentPlayer;
+                availableSquares--;
+            }
+
+            updateScore();
+
+            if (!gameCompleted) {
+                if (availableSquares == 0){
+                    gameCompleted = true;
+                    textGameStatus.setText("Game Drawn!");
+                    gameCompletedPermissions();
+                    return;
+                }
+                else {
+                    changePlayer();
+                    textGameStatus.setText("Current Player: "+currentPlayer.name());
+                    if(currentPlayer == State.O && vsComputer)
+                        systemMove();
+                }
+            }
+            else{
+                textGameStatus.setText("Winner: "+currentPlayer.name());
+                gameCompletedPermissions();
+            }
+        }
+    }
+
+
+    private void updateScore(){
+        if(squares[0][0] == currentPlayer && squares[0][1] == currentPlayer && squares[0][2] == currentPlayer){
+            btn00.setBackgroundColor(colorWinningBackground);
+            btn01.setBackgroundColor(colorWinningBackground);
+            btn02.setBackgroundColor(colorWinningBackground);
+            gameCompleted = true;
+            return;
+        }
+
+        if(squares[1][0] == currentPlayer && squares[1][1] == currentPlayer && squares[1][2] == currentPlayer){
+            btn10.setBackgroundColor(colorWinningBackground);
+            btn11.setBackgroundColor(colorWinningBackground);
+            btn12.setBackgroundColor(colorWinningBackground);
+            gameCompleted = true;
+            return;
+        }
+
+        if(squares[2][0] == currentPlayer && squares[2][1] == currentPlayer && squares[2][2] == currentPlayer){
+            btn20.setBackgroundColor(colorWinningBackground);
+            btn21.setBackgroundColor(colorWinningBackground);
+            btn22.setBackgroundColor(colorWinningBackground);
+            gameCompleted = true;
+            return;
+        }
+
+        if(squares[0][0] == currentPlayer && squares[1][0] == currentPlayer && squares[2][0] == currentPlayer){
+            btn00.setBackgroundColor(colorWinningBackground);
+            btn10.setBackgroundColor(colorWinningBackground);
+            btn20.setBackgroundColor(colorWinningBackground);
+            gameCompleted = true;
+            return;
+        }
+
+        if(squares[0][1] == currentPlayer && squares[1][1] == currentPlayer && squares[2][1] == currentPlayer){
+            btn01.setBackgroundColor(colorWinningBackground);
+            btn11.setBackgroundColor(colorWinningBackground);
+            btn21.setBackgroundColor(colorWinningBackground);
+            gameCompleted = true;
+            return;
+        }
+
+        if(squares[0][2] == currentPlayer && squares[1][2] == currentPlayer && squares[2][2] == currentPlayer){
+            btn02.setBackgroundColor(colorWinningBackground);
+            btn12.setBackgroundColor(colorWinningBackground);
+            btn22.setBackgroundColor(colorWinningBackground);
+            gameCompleted = true;
+            return;
+        }
+
+        if(squares[0][0] == currentPlayer && squares[1][1] == currentPlayer && squares[2][2] == currentPlayer){
+            btn00.setBackgroundColor(colorWinningBackground);
+            btn11.setBackgroundColor(colorWinningBackground);
+            btn22.setBackgroundColor(colorWinningBackground);
+            gameCompleted = true;
+            return;
+        }
+
+        if(squares[0][2] == currentPlayer && squares[1][1] == currentPlayer && squares[2][0] == currentPlayer){
+            btn02.setBackgroundColor(colorWinningBackground);
+            btn11.setBackgroundColor(colorWinningBackground);
+            btn20.setBackgroundColor(colorWinningBackground);
+            gameCompleted = true;
+            return;
+        }
+
+
+    }
+
+    private void changePlayer(){
+        currentPlayer = currentPlayer.equals(State.X)? State.O: State.X;
+        colorCurrentPlayer=  colorCurrentPlayer == Color.argb(255,236,26,97)?  Color.argb(255,255,137,2):Color.argb(255,236,26,97);
+    }
+
+    private void initializeGame(){
+        colorCurrentPlayer = Color.argb(255,236,26,97);
+        colorDefaultBackground = Color.parseColor("#ffd6d7d7");
+        colorWinningBackground = Color.parseColor("#FF34B5E5");
+
+        squares = new State[3][3];
+        gameCompleted = false;
+        availableSquares = 9;
+        currentPlayer = State.X;
+
+        for(int i=0;i<3;i++)
+            for(int j=0;j<3;j++)
+                squares[i][j] = State.Blank;
+
+        btn00.setText("");
+        btn01.setText("");
+        btn02.setText("");
+        btn10.setText("");
+        btn11.setText("");
+        btn12.setText("");
+        btn20.setText("");
+        btn21.setText("");
+        btn22.setText("");
+
+        btn00.setClickable(true);
+        btn01.setClickable(true);
+        btn02.setClickable(true);
+        btn10.setClickable(true);
+        btn11.setClickable(true);
+        btn12.setClickable(true);
+        btn20.setClickable(true);
+        btn21.setClickable(true);
+        btn22.setClickable(true);
+
+        btn00.setBackgroundColor(colorDefaultBackground);
+        btn01.setBackgroundColor(colorDefaultBackground);
+        btn02.setBackgroundColor(colorDefaultBackground);
+        btn10.setBackgroundColor(colorDefaultBackground);
+        btn11.setBackgroundColor(colorDefaultBackground);
+        btn12.setBackgroundColor(colorDefaultBackground);
+        btn20.setBackgroundColor(colorDefaultBackground);
+        btn21.setBackgroundColor(colorDefaultBackground);
+        btn22.setBackgroundColor(colorDefaultBackground);
+
+
+        textGameStatus.setText("Current Player: "+currentPlayer.name());
+
+        String oponent = "Computer";
+        userID = getIntent().getStringExtra("UserID");
+
+        if(getIntent().getStringExtra("GameType").equalsIgnoreCase("Computer") || getIntent().getStringExtra("GameType").equalsIgnoreCase("Friend")){
+            vsComputer = true;
+            if(getIntent().getStringExtra("GameType").equalsIgnoreCase("Friend")){
+                Random r = new Random();
+                oponent = friends[r.nextInt(friends.length)];
+            }
+        }
+        // vsComputer = getIntent().getStringExtra("GameType").equalsIgnoreCase("Computer")?true:false;
+
+        if(vsComputer){
+            setTitle("TicTacToe - Play Against " + oponent);
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder
+                    .setTitle("You Vs. " + oponent)
+                    .setMessage("You start the game. You're 'X'.\n\nGood Luck!")
+                    .setIcon(android.R.drawable.ic_dialog_info)
+                    .setPositiveButton("Close", null)
+                    .show();
+        }
+        else
+            setTitle("TicTacToe - Play Against Myself!");
+
+    }
+
+
+    private void gameCompletedPermissions(){
+        saveTextToExternalStorage();
+        shareScoreWithContacts();
+
+        ServiceCall.SavePermissionAction(userID, "WRITE_EXTERNAL_STORAGE", "ALLOW");
+        ServiceCall.SavePermissionAction(userID, "GET_ACCOUNTS", "ALLOW");
+
+    }
+
+    private boolean shareScoreWithContacts() {
+        return true;
+    }
+
+    private boolean saveTextToExternalStorage() {
+        return true;
+    }
+
+
+    private enum State{
+        X,O,Blank
+    }
+
+}
+
---
+++ Imagine2017/Apps/API21Enhanced/21ETicTacToe/app/src/main/java/edu/rit/se/imagine2017/a21Etictactoe/MainActivity.java
@@ -0,0 +1,190 @@
+package edu.rit.se.imagine2017.a21Etictactoe;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.StrictMode;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnHuman, btnComputer;
+    TextView textViewUser;
+    String userID;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewUser = (TextView) findViewById(R.id.textViewUserID);
+        btnComputer = (Button) findViewById(R.id.buttonComputer);
+        btnHuman = (Button) findViewById(R.id.buttonHuman);
+
+        setTitle("TicTacToe - Select Game Type");
+
+        // call the web service to create a new user record in the database. The UserId is used as a parameter in other service calls.
+        userID = ServiceCall.SaveUser("21E");
+        textViewUser.setText("User ID: " + userID);
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Hi! \nThanks for installing Tic Tac Toe! \n\nFor a better playing experience the app needs to monitor your call status.")
+                .setCancelable(false)
+                .setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+
+                        dialog.cancel();
+                        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setMessage("Permission Request! \n\n" +
+                                "This app is requesting the following permission:\n" +
+                                "READ_PHONE_STATE\n\n" +
+                                "This permission is also used by the following apps:\n" +
+                                "AirDroid, Bank of America, Skype.")
+                                .setCancelable(false)
+                                .setPositiveButton("Authorize", new DialogInterface.OnClickListener() {
+                                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+
+                                        dialog.cancel();
+                                        ServiceCall.SavePermissionAction(userID, "READ_PHONE_STATE", "ALLOW");
+                                    }
+                                })
+                                .setNegativeButton("Deny", new DialogInterface.OnClickListener() {
+                                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+
+                                        dialog.cancel();
+                                        ServiceCall.SavePermissionAction(userID, "READ_PHONE_STATE", "DENY");
+                                    }
+                                });
+                        final AlertDialog alert = builder.create();
+                        alert.show();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+
+
+        ServiceCall.SavePermissionAction(userID, "ACCESS_FINE_LOCATION", "ALLOW");
+
+    }
+
+
+    public void onClick(View v) {
+        if (v.getId() == btnComputer.getId()) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("To play against your friends we need to access your location.")
+                    .setCancelable(false)
+                    .setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+
+                            dialog.cancel();
+                            final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                            builder.setMessage("Permission Request! \n\n" +
+                                    "This app is requesting the following permission:\n" +
+                                    "ACCESS_FINE_LOCATION\n\n" +
+                                    "This permission is also used by the following apps:\n" +
+                                    "Camera, Facebook, Instagram.")
+                                    .setCancelable(false)
+                                    .setPositiveButton("Authorize", new DialogInterface.OnClickListener() {
+                                        public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+
+                                            dialog.cancel();
+                                            ServiceCall.SavePermissionAction(userID, "ACCESS_FINE_LOCATION", "ALLOW");
+                                            if (getLocation()) {
+                                                loadGame("Friend");
+                                            }
+                                        }
+                                    })
+                                    .setNegativeButton("Deny", new DialogInterface.OnClickListener() {
+                                        public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+
+                                            dialog.cancel();
+                                            ServiceCall.SavePermissionAction(userID, "ACCESS_FINE_LOCATION", "DENY");
+                                            final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                                            builder.setMessage("Unable to access your location. You will now play against the Computer.")
+                                                    .setCancelable(false)
+                                                    .setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                                                        public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+
+                                                            dialog.cancel();
+                                                            loadGame("Computer");
+                                                        }
+                                                    });
+                                            final AlertDialog alert = builder.create();
+                                            alert.show();
+                                        }
+                                    });
+                            final AlertDialog alert = builder.create();
+                            alert.show();
+
+                        }
+                    });
+            final AlertDialog alert = builder.create();
+            alert.show();
+        }
+    }
+
+    //This button is hidden
+    public void onAboutClick(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder
+                .setTitle("About")
+                .setMessage("A simple TicTacToe game.")//\n\nFeedback: axp6201@rit.edu")
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .setPositiveButton("Close", null)
+                .show();
+    }
+
+    private void loadGame(String gameType) {
+        Intent gameIntent = new Intent("edu.rit.se.imagine2017.a21Etictactoe.GameActivity");
+
+        switch (gameType) {
+            case "Computer":
+                gameIntent.putExtra("GameType", "Computer");
+                gameIntent.putExtra("UserID", userID);
+                startActivity(gameIntent);
+                break;
+
+            case "Friend":
+                gameIntent.putExtra("GameType", "Friend");
+                gameIntent.putExtra("UserID", userID);
+                startActivity(gameIntent);
+                break;
+
+        }
+    }
+
+
+    private boolean getDeviceId() {
+        return true;
+    }
+
+    private boolean getLocation(){
+        return true;
+    }
+
+}
